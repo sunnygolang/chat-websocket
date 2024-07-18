5ee74b5d156a0e632191e7310a994800143952c0
Eduardo Alexandre <sunnygolang@gmail.com>
2024-07-17 21:58:03 -0300

    feat: add bootstrap

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/sunnygolang/chat-websocket
 go 1.22.5
 
 require (
-	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
-	github.com/CloudyKit/jet/v6 v6.2.0 // indirect
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/CloudyKit/jet/v6 v6.2.0
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+	github.com/gorilla/websocket v1.5.3
 )
+
+require github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,12 +5,36 @@ import (
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
 var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
 	jet.InDevelopmentMode(),
 )
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
+}
 
 func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 	view, err := views.GetTemplate(tmpl)
@@ -30,6 +54,23 @@ func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected to endpoint")
+
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to Server</small></em>`
+
+	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Println(err)
 	}
